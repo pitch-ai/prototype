d419fd1dcc8f34aeb624f831ba3c64baafd14825
Anita Leung <leungak@cs.washington.edu>
2019-02-23 01:54:26 -0800

    Update semantic similarity to work with python 2.7, integrate into main prototype

--- pitch-ai-flask/flaskServer.py
+++ pitch-ai-flask/flaskServer.py
@@ -5,6 +5,11 @@ import speech_recognition as sr
 import re
 from flask import send_from_directory
 import os
+import urllib
+import urllib2
+import json
+import base64
+import requests
 
 dirname = os.path.dirname(__file__)
 UPLOAD_FOLDER = os.path.join(dirname, 'data')
@@ -21,11 +26,104 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Semantic similarity
+# Takes in a text string and outputs the understanding of the audience:
+# {
+#   "categories": ["/technology and computing/internet technology"], 
+#   "concepts": ["Copyright", "Copyright infringement"], 
+#   "emotions": "sadness", 
+#   "keywords": ["Copyright Act", "copyright violation"], 
+#   "sentiment": "negative"
+# }
+text_example = "The missiles and rockets are no longer flying in every direction. Nuclear testing has stopped. Some military facilities are already being dismantled. Our hostages have been released. And as promised, the remains of our fallen heroes are being returned home to lay at rest in American soil."
+api_key = 'Dx5l75uQ8lQC7bW3lwIYcpMmRsI46kKnlDWVme_8xqSs'
+service_endpoint = 'https://gateway-wdc.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-11-16'
+token_endpoint = 'https://iam.bluemix.net/identity/token'
+speech_to_text_endpoint = 'https://gateway-wdc.watsonplatform.net/speech-to-text/api/recognize'
+features = {'categories': {},
+            'concepts': {},
+            'emotion': {},
+            'entities':{},
+            'keywords': {},
+            'sentiment': {}} #all options: ['categories','concepts','emotion','entities','keywords','metadata','relations','semantic_roles','sentiment']
+concept_relevance_threshold = .8
+keyword_relevance_threshold = .8
+category_relevance_threshold = .9
+emotion_score_threshold = .65
+
+def perception(text):
+
+    data = {'features': features, 'text': text}
+    response = requests.post(url=service_endpoint, json=data, headers={'Authorization':'Bearer ' + get_access_token()})
+    if response.ok:
+        print(response.text)
+        print("")
+    else:
+        print("error")
+        print(response.content)
+
+    content = json.loads(response.content.decode())
+
+    print('Overall Sentiment: ' + content['sentiment']['document']['label'])
+
+    concepts = [concept['text'] for concept in content['concepts'] if concept['relevance'] > concept_relevance_threshold]
+    print('Concepts include: ' + str(concepts))
+
+    keywords = [keyword['text'] for keyword in content['keywords'] if keyword['relevance'] > keyword_relevance_threshold]
+    print('Keywords noted: ' + str(keywords))
+
+    categories = [category['label'] for category in content['categories'] if category['score'] > category_relevance_threshold]
+    print('Categories: ' + str(categories))
+
+    emotions = max(content['emotion']['document']['emotion'].items(), key=lambda k: k[1])
+    if emotions[1] > emotion_score_threshold:
+        print("Detected emotion of speech is " + emotions[0])
+
+
+    data = {
+        'categories': categories,
+        'concepts': concepts,
+        'emotions': emotions[0],
+        'keywords': keywords,
+        'sentiment': content['sentiment']['document']['label']
+    }
+
+    return json.dumps(data)
+    print(json_string)
+
+def get_access_token():
+    data = {'grant_type': 'urn:ibm:params:oauth:grant-type:apikey',
+            'apikey': api_key}
+    # data = parse.urlencode(data).encode()
+    data = urllib.urlencode(data)
+
+    req = urllib2.Request(token_endpoint, data=data)
+    req.add_header('Accept', 'application/json')
+    req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+
+    try:
+        response = urllib2.urlopen(req)
+        # encoding = response.info().get_content_charset('utf8')  # JSON default
+        # encoding = response.headers.getparam("charset")  # python 2
+        # response_data = json.loads(response.read().decode(encoding))
+        response_data = json.loads(response.read())
+        return response_data['access_token']
+    except urllib2.HTTPError as err:
+       if err.code == 400:
+           print("HTTP 400 Bad Request, token request is invalid")
+       elif err.code == 404:
+           print("HTTP 404 Not Found, token request is missing something")
+       elif err.code == 401:
+            print("HTTP 401 Unauthorized, make sure you are using valid credentials to make request.")
+       else:
+           raise
+
+
+# Filler words
 def get_filler_word_count(filler_word,text):
     return sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(filler_word), text))
 
 def get_text_from_speech(audio_file):
-    print("Hi")
     print(str(audio_file))
     r = sr.Recognizer()
     af = sr.AudioFile(audio_file)
@@ -66,7 +164,9 @@ def upload_file():
             text = get_text_from_speech(file_location)
             filler_word ="like"
             like_count = get_filler_word_count(filler_word,text)
-            flash(" Transcript: " + str(text) +"\n"+ "Like count : "+ str(like_count))
+            flash(" Transcript: " + str(text) + "</br>" +
+                "Like count : " + str(like_count) + "</br>" + 
+                "Semantic similarity: " + str(perception(text)))
         else:
             flash('Error: Problem processing the file')
     return render_template('hello.html', form=form)
