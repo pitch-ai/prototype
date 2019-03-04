34c49090ca3c72309233a9a6d73950bf186979e4
Anita Leung <leungak@cs.washington.edu>
2019-03-03 22:02:02 -0800

    Add loading page

--- pitch-ai-flask/static/js/script.js
+++ pitch-ai-flask/static/js/script.js
@@ -5,3 +5,12 @@ console.log("clicked");
  var inputFile = document.getElementById("inputFile");
  inputFile.click();
 });
+
+ // Show spinner if we can successfully analyze file
+$("#analyze-btn").click(function() {
+ console.log("analyze clicked!!")
+ $(".new-presentation").hide();
+ $("#jumbotron-presentation").css("visibility", "hidden");
+ $(".navbar").addClass("navbar-dropshadow");
+ $("#upload-loading").show();
+});
